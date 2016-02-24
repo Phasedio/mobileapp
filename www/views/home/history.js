cf8d34a2e76fc75d82d3d7358782893e2cbb3d7d
clouie87 <carol.louie@gmail.com>
2016-02-23 22:37:24 -0800

    why ng-if doesnt work??

--- www/views/home/history.js
+++ www/views/home/history.js
@@ -1,23 +1,28 @@
 'Use Strict';
-angular.module('App').controller('historyController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup,$ionicModal, $ionicSideMenuDelegate, $firebaseObject, Auth, FURL, Utils,Phased, $stateParams) {
+
+
+angular.module('App').controller('historyController', function ($scope, $filter, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup,$ionicModal, $ionicSideMenuDelegate, $firebaseObject, Auth, FURL, Utils,Phased, $stateParams) {
   var ref = new Firebase(FURL);
-  $scope.status = Phased.team.statuses;
-  console.log('the status is ', $scope.status)
-  //console.log($stateParams, $stateParams.userid)
+
+
+
   $scope.userid = $stateParams.userid;
   console.log($scope.userid);
+  $scope.currentUser = Phased.user;
+  console.log($scope.currentUser);
+
 
   $scope.histories = Phased.team.statuses;
-  //$scope.history1 = Phased.team.statuses[$stateParams.userid]
-  //console.log('the history is', $scope.history, $scope.history1)
+  console.log('the history', $scope.histories);
 
   angular.forEach($scope.histories, function(key, value){
     //console.log('the key is:', key, 'and the value is:', value);
     //var user = key.user;
     //console.log(user, $scope.userid);
     if (key.user == $scope.userid){
-      console.log('what is the user id', $scope.userid, key)
+      //console.log('what is the user id', $scope.userid, key);
       $scope.history = key;
+      console.log($scope.history)
     }
 
 
