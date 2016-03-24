c23859387fb419c7138a3be9b1f86f29c3959a3a
Brian Best <brian.brianb64@gmail.com>
2016-03-24 10:43:08 -0300

    Changed task name to to-do, model looks like status model now

--- www/views/tasks/tasks.js
+++ www/views/tasks/tasks.js
@@ -1,4 +1,30 @@
 'Use Strict';
+
+
+
+
+angular.module('App').directive('focusMe', function($timeout, $parse) {
+  return {
+    //scope: true,   // optionally create a child scope
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.focusMe);
+      scope.$watch(model, function(value) {
+        console.log('value=',value);
+        if(value === true) {
+          $timeout(function() {
+            element[0].focus();
+          });
+        }
+      });
+      // to address @blesh's comment, set attribute value to 'false'
+      // on blur event:
+      element.bind('blur', function() {
+         console.log('blur');
+         //scope.$apply(model.assign(scope, false));
+      });
+    }
+  };
+});
 /**
   *
   * allows ordering an object as if it were an array,
